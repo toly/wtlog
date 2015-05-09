4e8ac0f7c15b18d40f4e23c786fda55020565cbd
toly <tolytrader@gmail.com>
2015-05-09 22:28:33 +0300

    implementation init app dir and config file

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 #!/usr/bin/env python
 __author__ = 'toly'
 
+import os
+import commands
 from datetime import datetime
 from argparse import ArgumentParser, ArgumentTypeError
+
+
+HOME_DIR = os.path.expanduser('~')
+APP_DIR = os.path.join(HOME_DIR, '.wtlog')
+CONFIG = os.path.join(APP_DIR, 'projects.conf')
 
 
 def valid_date(s):
@@ -14,7 +21,7 @@ def valid_date(s):
 
 
 def create_arg_parser():
-    parser = ArgumentParser()
+    parser = ArgumentParser(prog='wtlog')
     parser.add_argument('-i', '--init', action="store_true",
                         help="Init WTLog:\n 1. make dirs\n 2. find projects (.git/)\n 3. update crontab")
     parser.add_argument('-l', '--log', action="store_true",
@@ -25,10 +32,58 @@ def create_arg_parser():
     return parser
 
 
+def get_projects():
+    with open(CONFIG) as f:
+        lines = f.readlines()
+    lines = map(lambda x: x.strip(), lines)
+    lines = filter(None, lines)
+    return lines
+
+
+def make_app_dirs():
+    if not os.path.exists(APP_DIR):
+        os.mkdir(APP_DIR)
+
+
+def add_projects():
+    project_path = True
+    projects = list()
+    while project_path:
+        project_path = raw_input('Add project path: ')
+        if project_path:
+            if os.path.exists(project_path):
+                if os.path.isdir(project_path):
+                    projects.append(project_path)
+                else:
+                    print 'Error: Project path must be a directory: %s' % project_path
+            else:
+                print 'Error: Project path must be exists: %s' % project_path
+
+    with open(CONFIG, 'w') as f:
+        for project_path in projects:
+            f.write(project_path + '\n')
+
+
 def main():
     arg_parser = create_arg_parser()
     args = arg_parser.parse_args()
 
+    if args.init:
+        make_app_dirs()
+        add_projects()
+        # update crontab
+        return
+
+    if args.log:
+        # write log
+        return
+
+    if args.report:
+        report_date = args.date or datetime.now()
+        # make and print report
+        return
+
+    arg_parser.print_help()
 
 if __name__ == '__main__':
     main()

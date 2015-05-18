01dccf811ea16d03ebd52c9ff19105eff9597c82
toly <tolytrader@gmail.com>
2015-05-18 10:46:14 +0300

    make install script

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='wtlog',
+    version='0.0.1',
+    packages=['wtlog'],
+    scripts=['wtlog/wtlog.py'],
+    url='https://github.com/toly/wtlog',
+    license='',
+    author='toly',
+    author_email='tolytrader@gmail.com',
+    description='Util for auto logging work time'
+)
